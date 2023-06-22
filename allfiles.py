abc2eb0a17d7116e39989356b84b4a75e2194eda
Sithu Kaungset <sithukaungset@Sithus-MacBook-Air.local>
2023-06-23 04:15:51 +0900

    override prompt and natural model

--- allfiles.py
+++ allfiles.py
@@ -71,33 +71,48 @@ def main():
     embeddings = OpenAIEmbeddings(
         deployment=OPENAI_EMBEDDING_DEPLOYMENT_NAME, model=OPENAI_EMBEDDING_MODEL_NAME, chunk_size=1)
 
-    # system_message = {"You are an AI assistant that tells jokes"}
-    # system_message_prompt = SystemMessagePromptTemplate.from_template(
-    #     system_message)
-    # human_template = "{text}"
-    # human_message_prompt = HumanMessagePromptTemplate.from_template(
-    #     human_template)
-    # chat_prompt = ChatPromptTemplate.from_messages(
-    #     [system_message_prompt])
-
-    # chain = LLMChain(llm=llm, prompt=chat_prompt)
-
     # Select chat mode
     natural_chat_mode = st.checkbox('Switch to Natural Chat Mode')
 
     if natural_chat_mode:
+        prompt_template = st.text_input("Custom Prompt 🎯:")
         user_input = st.text_input("Type your message here 🤖:")
+        # Create a placeholder for the chat history
+        chat_placeholder = st.empty()
+
+        # Fetch all records from the database
+        c.execute("SELECT * FROM chat_history")
+        rows = c.fetchall()
+
+        # Display the chat history
+        chat_history = "<h2>Chat History:</h2>"
+        for row in rows:
+            st.markdown(f"<strong>User :</strong> {row[0]}<br><strong>ChatBot :</strong> {row[1]}<br><br>",
+                        unsafe_allow_html=True)
+
+        chat_placeholder.markdown(chat_history, unsafe_allow_html=True)
+
         if user_input:
-            # response = chain.run(f"Tell me a dad joke")
-            response = "I am sithu"
-            # st.markdown(
-            #     f'### Answer: \n {response["choices"][0]["message"]["content"]}', unsafe_allow_html=True)
-            # # Insert the question and answer into the database
-            # c.execute("INSERT INTO chat_history VALUES (?,?)",
-            #           (user_input, response["choices"][0]["message"]["content"]))
+            response = openai.ChatCompletion.create(
+                engine=OPENAI_DEPLOYMENT_NAME,
+                messages=[
+                    {"role": "system",
+                     # "content": "Assistant is a large language model trained by OpenAI."},
+                     "content": prompt_template},
+                    {"role": "user", "content": user_input}
+                ]
+            )
+            st.markdown(
+                f'### Answer: \n {response["choices"][0]["message"]["content"]}', unsafe_allow_html=True)
+            # Insert the question and answer into the database
+            c.execute("INSERT INTO chat_history VALUES (?,?)",
+                      (user_input, response["choices"][0]["message"]["content"]))
 
             # Commit the insert
             conn.commit()
+            # Update the chat history with the new message
+            chat_history = f"<strong>User :</strong> {user_input}<br><strong>ChatBot :</strong> {response['choices'][0]['message']['content']}<br><br>" + chat_history
+            chat_placeholder.markdown(chat_history, unsafe_allow_html=True)
 
     else:
 
@@ -154,6 +169,17 @@ def main():
 
             # show user input
             user_question = st.text_input("Ask a question 🤖:")
+
+            # Fetch all records from the database
+            c.execute("SELECT * FROM chat_history")
+            rows = c.fetchall()
+
+            # Display the chat history
+            st.markdown("<h2>Chat History:</h2>", unsafe_allow_html=True)
+            for row in rows:
+                st.markdown(f"<strong>User :</strong> {row[0]}<br><strong>Chat Bot :</strong> {row[1]}<br><br>",
+                            unsafe_allow_html=True)
+
             if user_question:
                 result = qa({"query": user_question})
                 # Display the result in a more noticeable way
