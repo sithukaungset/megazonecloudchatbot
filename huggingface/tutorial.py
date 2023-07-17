2ad6ab0a9631948880e49c220100f469051629b4
Sithu Kaungset <sithukaungset@Sithus-MacBook-Air.local>
2023-07-17 16:58:57 +0900

    tabular data modified

--- huggingface/tutorial.py
+++ huggingface/tutorial.py
@@ -5,5 +5,19 @@ API_URL = "https://api-inference.huggingface.co/models/gpt3"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 def query(payload):
 	data = json.dumps(payload)
-	response = requests.request("POST", API_URL, header)
+	response = requests.request("POST", API_URL, headers= headers, data=data)
+	
+	return json.loads(response.content.decode("utf-8"))
+data = query("Can you please let us know more details about your ")
 
+import gradio as gr
+
+def greet(name):
+	return "Hello" + name + "!"
+
+demo = gr.Interface(fn= greet, inputs="text", outputs="text")
+# to launch the demo 
+demo.launch()
+
+
+ 
