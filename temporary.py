d917f95c84cc6d9e444e099c9921b55f89ab944e
sithuAOAI <sithu@mz.co.kr>
2023-09-22 17:22:52 +0900

    fix pretty frontend 2

--- temporary.py
+++ temporary.py
@@ -1,39 +1,3 @@
-from dotenv import load_dotenv
-import os
-import pandas as pd
-import streamlit as st
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain.chat_models import AzureChatOpenAI
-import openai
-from langchain import LLMChain
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from pptx import Presentation
-from langchain.llms import AzureOpenAI
-import tiktoken
-import sqlite3
-import fitz  # PyMuPDF
-import pytesseract
-from pdfminer.high_level import extract_text
-from pdf2image import convert_from_path
-import re
-#import cv2
-import base64
-import requests
-import json
-import sympy as sp
-import io
-from pptx.util import Inches
-from PIL import Image
-
-
 #Powerpoint Processor
 class PowerPointProcessor:
 
@@ -145,9 +109,7 @@ class PDFProcessor:
         return text
     
     def remove_headers_and_footers(self, text):
-        # A very basic method: remove the first and last line from each page, assuming they might be headers/footers.
-        # This might need more advanced logic, possibly using patterns or machine learning models.
-        # Split text into pages
+
         pages = text.split("\n\n")
         # For each page, remove the first and last lines if they exist
         cleaned_pages = []
@@ -249,10 +211,6 @@ class PDFProcessor:
             processed_expressions[expression] = expression_data
         
         return processed_expressions
-    
-    # Now when we call 'process math expressions', it will return a dictionary with each original
-    # expression as the key.The value for each key will be another dictionary containing the evaluated,
-    # simplified and LaTex transcribed results.
 
 
     def process_pdf_stream(self, pdf_stream):
@@ -286,19 +244,6 @@ class TabularDataProcessor:
         self.data = self.data.applymap(lambda s: s.lower() if type(
             s) == str else s)  # convert text to lowercase
 
-    # def transform_to_sentences(self):
-    #     # Create a list to store the sentences
-    #     sentences = []
-
-    #     # Iterate over each row in the DataFrame
-    #     for index, row in self.data.iterrows():
-    #         # Create a sentence for each row
-    #         sentence = ','.join(
-    #             [f'{col} is {val}' for col, val in row.items()])
-    #         sentences.append(sentence)
-
-    #     return sentences
-    
     def transform_to_sentences(self):
         # Create a list to store the sentences
         sentences = []
@@ -355,100 +300,7 @@ class TabularDataProcessor:
             text += " .".join(self.transform_to_sentences()) + ". "
 
         return text
-    
-    
 
-    # MATHPIX_ENDPOINT = "https://api.mathpix.com/v3/text"
-    # MATHPIX_HEADERS = {
-    #     "app_id": "MY_APP_ID",
-    #     "app_key": "MY_APP_KEY",
-    #     "Content-type": "application/json"
-    # }
-
-    # def ocr_with_mathpix(self, image):
-    #     """
-    #     Use Mathpix API to extract text from the given image.
-    #     """
-    #     # Convert the image to a base64 string
-    #     _, img_encoded = cv2.imencode('.png', image)
-    #     img_str = base64.b64encode(img_encoded).decode('utf-8')
-
-    #     # Create payload for API request
-    #     payload = {
-    #         "src": f"data:image/png;base64, {img_str}"
-    #     }
-    #     response = requests.post(
-    #         self.MATHPIX_ENDPOINT, headers=self.MATHPIX_HEADERS, data=json.dumps(payload))
-    #     response_data = response.json()
-
-    #     # Extract text from the response, handle errors appropriately
-    #     return response_data('text', '')
-
-    # def ocr_pdf(self, pdf_path):
-    #     """
-    #     Convert a PDF into images and then use Mathpix to extract text.
-    #     Return the combined text from all pages.
-    #     """
-
-    #     # Convert PDF to a list of images
-    #     images = convert_from_path(pdf_path)
-
-    #     # OCR each image to extract text using Mathpix
-    #     texts = [self.ocr_with_mathpix(img) for img in images]
-
-    #     # Combine the texts from all pages
-    #     combined_text = "/n".join(texts)
-
-    #     return combined_text
-
-    # def ocr_pdf(self, pdf_path):
-    #     """ 
-    #     Convert a PDF into images and then use OCR to extract text.
-    #     Return the combined text from all pages.
-    #     """
-
-    #     # Convert PDF to a list of images
-    #     images = convert_from_path(pdf_path)
-
-    #     # OCR each image to extract text
-    #     texts = [pytesseract.image_to_string(img) for img in images]
-
-    #     # Combine the texts from all pages
-    #     combined_text = "/n".join(texts)
-
-    #     return combined_text
-
-    # def process_pdf(self, pdf_path):
-    #     """
-    #     Process a PDF file using OCR and then preprocess any potential mathematical expressions.
-    #     """
-    #     # Extract text from the PDF using OCR
-    #     ocr_text = self.ocr_pdf(pdf_path)
-
-    #     # Split the OCR text into lines and preprocess each line individually
-    #     preprocessed_lines = [self.preprocess_math_expression(
-    #         line) for line in ocr_text.split("n")]
-
-    #     # Combined the preprocessed lines back into a single string
-    #     preprocessed_text = "\n".join(preprocessed_lines)
-
-    #     return preprocessed_text
-
-    # def preprocess_math_expression(self, expression):
-    #     # Check if the input is a string, if not, return as is
-    #     if not isinstance(expression, str):
-    #         return expression
-
-    #     # Remove any extra spaces
-    #     expression = expression.lower().strip()
-
-    #     # Add spaces around operators for better tokenization
-    #     expression = re.sub(r'(\+|\-|\*|\/|\=|\(|\))', r' \1 ', expression)
-
-    #     # Remove any extra spaces around numbers and variables
-    #     expression = re.sub(r'/s+', ' ', expression).strip()
-
-    #     return expression
 
 
 def translate(text, target_language='ko'):
@@ -540,22 +392,9 @@ def main():
                     {"role": "user", "content": user_input}
                 ]
             )
-            # st.markdown(
-            #     f'### Answer: \n {response["choices"][0]["message"]["content"]}', unsafe_allow_html=True)
-            
-            # Pretty front-end
+
             response_content = response["choices"][0]["message"]["content"]
-            
-            # Update the chat history with the new message
-            chat_history += f"<strong>User :</strong> {user_input}<br><strong>ChatBot :</strong> {response_content}<br><br>"
-            chat_placeholder.markdown(chat_history, unsafe_allow_html=True)
-
-            # Insert the question and answer into the database
-            c.execute("INSERT INTO chat_history VALUES (?,?)", (user_input, response_content))
-            conn.commit()
-
-
-
+           
             if st.button('Translate to Korean'):
                 translated_text = translate(result)
             # Insert the question and answer into the database
@@ -607,12 +446,8 @@ def main():
                 with st.spinner('Reading the Excel file...'):
                     excel_file = pd.ExcelFile(uploaded_file)
 
-                    # text = process_all_sheets(excel_file)
-                    # Create an instance of TabularDataProcessor
                     processor = TabularDataProcessor(None)
-                    # df = pd.read_excel(uploaded_file)
 
-                    # Get the number of sheets and their names
                     num_sheets = processor.get_num_sheets(excel_file)
                     sheet_names = processor.get_sheet_names(excel_file)
 
@@ -623,13 +458,6 @@ def main():
 
                     text = processor.process_all_sheets(excel_file)
 
-                    # for sheet in sheet_names:
-                    #     df = pd.read_excel(excel_file, sheet_name=sheet)
-                    #     processor.data = df  # Set the data for the processor
-                    #     processor.preprocess()
-                    #     # text = " ".join(map(str, df.values))
-                    #     text = ". ".join(
-                    #         processor.transform_to_sentences()) + ""
 
             elif file_details["FileType"] == "text/csv":
                 with st.spinner('Reading the CSV file...'):
@@ -651,12 +479,9 @@ def main():
             # use the faiss vector store we saved to search the local document
             retriever = vectorStore.as_retriever(
                 search_type="similarity", search_kwargs={"k": 2})
-
-            # use the vector store as a retriever
             qa = RetrievalQA.from_chain_type(
                 llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=False)
 
-            # show user input
             prompt_template = st.text_input("Custom Prompt 🎯:")
             user_question = st.text_input("Ask a question 🤖:")
 
@@ -676,11 +501,7 @@ def main():
 
             if user_question:
                 result = qa({"query": user_question})
-                # Display the result in a more noticeable way
-                
-                # st.markdown(
-                #     f'### Answer: \n {result["result"]}', unsafe_allow_html=True)
-
+               
                 response_content = result["result"]
                 response_id = f"response_{hash(response_content)}"
                 st.markdown(f"""
@@ -702,13 +523,9 @@ def main():
                     </script>
                 """, unsafe_allow_html=True)
 
-
-
-                # Insert the question and answer into the database
                 c.execute("INSERT INTO chat_history VALUES (?,?)",
                           (user_question, result["result"]))
 
-                # Commit the insert
                 conn.commit()
                 chat_history = f"<strong>User :</strong> {user_question}<br><strong>ChatBot :</strong> {result['result']}<br><br>" + chat_history
                 chat_placeholder.markdown(chat_history, unsafe_allow_html=True)
